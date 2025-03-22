5bd557190e47cf631cfefc5238a7e7756dbd4816
windybranch <windybranch@icloud.com>
2025-03-21 20:24:19 -0700

    refactor: improve article view styling

--- lib/ui/articles_view.dart
+++ lib/ui/articles_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 
 import '../logic/categories.dart';
 
@@ -15,12 +16,73 @@ class ArticlesView extends StatelessWidget {
         (context, index) {
           final item = category.articles[index];
 
-          return ListTile(
-            title: Padding(
-              padding: const EdgeInsets.only(bottom: 4.0),
-              child: Text(item.title),
-            ),
-            subtitle: Text(item.group),
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(children: [
+              Flexible(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Chip(
+                          label: Text(item.group),
+                          padding: EdgeInsets.symmetric(horizontal: 2.0),
+                          visualDensity: VisualDensity.compact,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(100.0),
+                          ),
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Colors.grey.shade200,
+                          radius: 14,
+                          child: IconButton(
+                            padding: EdgeInsets.zero,
+                            iconSize: 16,
+                            icon: Icon(
+                              LucideIcons.check,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    ),
+                    Flexible(
+                      child: Text(
+                        item.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 24,
+                          letterSpacing: -1.0,
+                          height: 1.2,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.grey.shade200,
+                          radius: 14,
+                          child: IconButton(
+                            padding: EdgeInsets.only(top: 4),
+                            iconSize: 20,
+                            icon: Icon(
+                              LucideIcons.chevronDown,
+                              color: Colors.black87,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ]),
           );
         },
       ),
